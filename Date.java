ab974457337610491e043088320bb8ebe5f0014a
mycinus <45410115+mycinus@users.noreply.github.com>
2019-03-14 18:39:15 -0700

    Add files via upload

--- Date.java
+++ Date.java
@@ -2,5 +2,13 @@ package com.example.matthew.calendarapp;
 
 public class Date {
 
+    private String date;
+
+    public Date(String date)
+    {
+        this.date = date;
+    }
+
+    public String getDate() {return date;}
 
 }
